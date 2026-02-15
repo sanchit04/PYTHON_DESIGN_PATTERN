a4827511aa7060515edd085f2e9b32640fc9ed32
Sanchit <sanchit10gawde@gmail.com>
2026-02-15 21:47:53 +0530

    DESIGN PATTERN

--- messy_code.py
+++ messy_code.py
@@ -91,7 +91,7 @@ class NotificationSystem:
             print("[SUCCESS] sent successfully")
 
 
-        # METRICS
+        # METRICS are
         print("[Metrics] Notification Processed")
         return True
 
