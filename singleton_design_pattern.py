9881f8b530230c803dabf1256062d5adb910f7ec
Sanchit <sanchit10gawde@gmail.com>
2026-02-15 20:15:42 +0530

    DESIGN PATTERN

--- singleton_design_pattern.py
+++ singleton_design_pattern.py
@@ -18,6 +18,8 @@ from abc import ABC,abstractmethod
 import random
 import time
 
+
+# ALL THE LOGGING CAN BE NOW CONTROLLED FROM ONE SINGLE CLASS!
 class Logger:
     # CLASS Variable is instance
     _instance=None
@@ -42,6 +44,8 @@ class Logger:
     def success(self,message:str):
         print(f"[SUCCESS] {message}")
 
+#Setting logger at global level!
+logger = Logger()
 
 # STRATEGY PATTERN
 class NotificationStrategy(ABC):
@@ -53,25 +57,25 @@ class EmailNotification(NotificationStrategy):
     def send(self,recipient,message):
         #Validation
         if not "@" in recipient:
-            print("[ERROR] invalid recipient for email notification")
+            logger.error("invalid recipient for email notification")
         #SendGRIDAPI Implementation
-        print("[LOG] using SEND GRID API to send email")
-        print(f"[sendgrid] sent mail to recipient:{recipient} with message:{message}")
+        logger.log("using SEND GRID API to send email")
+        logger.success(f"sent mail to recipient:{recipient} with message:{message}")
 
 class SMSNotification(NotificationStrategy):
     def send(self,recipient,message):
         #Validation
         if not recipient.isdigit():
-            print("[ERROR] invalid recipient for sms notification")
+            logger.error("invalid recipient for sms notification")
         #TwilioAPI implementation
-        print("[LOG] using Twillio API to send email")
-        print(f"[twillio] sent message to recipient:{recipient} with message:{message}")
+        logger.log("using Twillio API to send email")
+        logger.success(f"sent message to recipient:{recipient} with message:{message}")
 
 class PushNotification(NotificationStrategy):
     def send(self,recipient,message):
         #FireBase API implementation
-        print("[LOG] using Firebase API to send email")
-        print(f"[Firebase] sent message to recipient:{recipient} with message:{message}")
+        logger.log("using Firebase API to send email")
+        logger.success(f"sent message to recipient:{recipient} with message:{message}")
 
 #FACTORY TO CENTRALIZE OBJECT CREATION AND MOVE THAT FROM MAIN TO HERE:
 class NotificationFactory:
@@ -93,13 +97,13 @@ class NotificationSystem:
     def send_notification(self,notification_type:str,recipient:str,message:str):
 
         # Logging
-        print(f"[LOG] Preparing to send notification_type : {notification_type} to {recipient}")
+        logger.log(f"Preparing to send notification_type : {notification_type} to {recipient}")
 
         strategy = NotificationFactory.create_notification(notification_type=notification_type)
 
         # Basic Validation
         if not recipient or not message:
-            print("[ERROR] Invalid Input")
+            logger.error("Invalid Input")
             return False
 
         success=strategy.send(recipient=recipient,message=message)
@@ -108,24 +112,20 @@ class NotificationSystem:
 
         # Retry Logic (Still messy)
         if random.choice([True,False]):
-            print("[WARNING] Temporary failure retrying...")
+            logger.warning("Temporary failure retrying...")
             time.sleep(1)
-            print("[SUCCESS] sent after retry")
+            logger.success("sent after retry")
         else:
-            print("[SUCCESS] sent successfully")
+            logger.success("sent successfully")
 
         # METRICS
-        print("[Metrics] Notification Processed")
+        logger.success("Notification Processed")
 
         return True
 
 
 if __name__=='__main__':
     system = NotificationSystem()
-    # NOT REQUIRED ANYMORE AS WE IMPLEMENTED FACTORY DESIGN PATTERN
-    # email_strategy = EmailNotification()
-    # sms_strategy = SMSNotification()
-    # push_strategy = PushNotification()
 
     system.send_notification(notification_type='email',recipient="sanchit10gawde@gmail.com",message="Hello I am email")
     system.send_notification(notification_type='sms',recipient="9029187708",message="Hello I am text message")
